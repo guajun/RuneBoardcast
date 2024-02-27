e39004acd7070f32b2dbf400d0ec63bb21a3bc67
gua_jun <whgls323232@gmail.com>
2024-02-27 10:20:37 +0800

    done discord send message

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,69 @@
 #include <Arduino.h>
+#include <WiFi.h>
+// Header file of ssid, password and discord hook
+#include "Secret.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#include <HTTPClient.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#include <WiFiClientSecure.h>
+
+
+const char server[] = "discord.com";
+const int port = 443;
+
+// WiFiClient client;
+// WiFiClientSecure client;
+// HttpClient http_client = HttpClient(client, server, port);
+
+void setup()
+{
+    delay(5000);
+    Serial.begin(115200);
+    WiFi.begin(SSID, PWD);
+    WiFi.mode(WIFI_MODE_STA);
+    delay(5000);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        WiFi.reconnect();
+        Serial.print("Connecting to WiFi: ");
+        Serial.print(SSID);
+        Serial.println();
+        delay(1000);
+    }
+
+    Serial.print("WiFI Connected to ");
+    Serial.print(SSID);
+    Serial.println();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+const char json[] PROGMEM =
+    R"rawliteral(
+{"content":"This is a message from ESP32"}
+        )rawliteral";
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void loop()
+{
+    WiFiClientSecure *client = new WiFiClientSecure;
+
+    if (client)
+    {
+        // just ignore it
+        client->setInsecure();
+        {
+            HTTPClient https;
+
+            Serial.print("Hello");
+            Serial.println();
+
+            String content = "This is a message from ESP32";
+            https.begin(*client, DISCORD_HOOK);
+            https.addHeader("Content-Type", "application/json");
+            https.POST(json);
+            https.end();
+        }
+        delete client;
+    }
+
+    delay(1000);
 }
